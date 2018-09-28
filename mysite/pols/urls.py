2046b56008d8599ae4f88fd945817280d16d0211
Mansi <mansi@webllisto.com>
2018-09-28 18:12:50 +0530

    initial commit

--- mysite/pols/urls.py
+++ mysite/pols/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from pols.views import index
+from . import views
 
 urlpatterns = [
-	path('index/',index, name="Hello, world. You're at the polls index."),
+	path('',views.index, name="Hello, world. You're at the polls index."),
+	path('<int:question_id>/', views.detail, name='detail'),
+	path('<int:question_id>/results/', views.results, name='results'),
+	path('<int:question_id>/vote/', views.vote, name='vote'),
+
 ]
